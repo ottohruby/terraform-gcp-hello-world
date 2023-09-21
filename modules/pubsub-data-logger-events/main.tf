8369725393adeb2cdf60f58df78c87fdbbb65914
ottohruby <70574289+ottohruby@users.noreply.github.com>
2023-09-21 22:17:57 +0200

    Update main.tf

--- modules/pubsub-data-logger-events/main.tf
+++ modules/pubsub-data-logger-events/main.tf
@@ -11,12 +11,21 @@ resource "google_apikeys_key" "maps" {
   }
 }
 
+resource "google_pubsub_topic" "example" {
+  name = "data-logger-events"
+  message_retention_duration = "86600s"
+}
+
 resource "google_pubsub_topic_iam_binding" "binding" {
-  project = google_project.basic.project_id
+  project = "otto-hruby-test"
   topic = "data-logger-events"
   role = "roles/pubsub.publisher"
   members = [
     "allUsers"
+  ]
+
+  depends_on = [
+    google_pubsub_topic.example
   ]
 }
 
