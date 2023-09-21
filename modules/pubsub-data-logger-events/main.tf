a5e1afdaf27e0a90cfacc1e9897a01904d261cac
ottohruby <70574289+ottohruby@users.noreply.github.com>
2023-09-21 22:52:02 +0200

    Update main.tf

--- modules/pubsub-data-logger-events/main.tf
+++ modules/pubsub-data-logger-events/main.tf
@@ -29,6 +29,19 @@ resource "google_pubsub_topic_iam_binding" "binding" {
   ]
 }
 
+
+resource "google_pubsub_subscription" "example" {
+  name  = "example-subscription"
+  topic = google_pubsub_topic.example.name
+
+  message_retention_duration = "1800s" # 30 minutes
+
+  ack_deadline_seconds = 20
+
+  enable_message_ordering    = false
+}
+
+
 #data = resource.google_apikeys_key.maps-api-key.key_string
 
 
