1f08fc909f8e5f7caca65822c0753ecddffefa04
ottohruby <70574289+ottohruby@users.noreply.github.com>
2023-10-01 20:26:46 +0200

    Update main.tf

--- modules/data_logger/main.tf
+++ modules/data_logger/main.tf
@@ -6,51 +6,20 @@ resource "google_service_account" "service_account" {
   account_id   = "data-logger"
 }
 
-resource "google_service_account_iam_binding" "admin-account-iam" {
-  service_account_id = google_service_account.service_account.name
-  role               = "roles/pubsub.publisher"
-}
-
-resource "google_cloud_run_service" "default" {
-  project = "otto-hruby-test"
-  name     = "hello"
-  location = "europe-west1"
+resource "google_cloud_run_v2_service" "default" {
+  name     = "otto-hruby-test"
+  location = "hello2"
+  ingress = "INGRESS_TRAFFIC_ALL"
 
   metadata {
     namespace = "otto-hruby-test"
   }
 
-  service_account_name = google_service_account.service_account.name
-
   template {
-    spec {
-      containers {
-        image = "us-docker.pkg.dev/cloudrun/container/hello"
-      }
+    containers {
+      image = "us-docker.pkg.dev/cloudrun/container/hello"
     }
   }
-
-  traffic {
-    percent         = 100
-    latest_revision = true
-  }
-}
-
-data "google_iam_policy" "noauth" {
-  binding {
-    role = "roles/run.invoker"
-    members = [
-      "allUsers",
-    ]
-  }
-}
-
-resource "google_cloud_run_service_iam_policy" "noauth" {
-  location    = "europe-west1"
-  project     = "otto-hruby-test"
-  service     = google_cloud_run_service.default.name
-
-  policy_data = data.google_iam_policy.noauth.policy_data
 }
 
 resource "google_cloud_run_domain_mapping" "default" {
@@ -63,7 +32,7 @@ resource "google_cloud_run_domain_mapping" "default" {
   }
 
   spec {
-    route_name = google_cloud_run_service.default.name
+    route_name = google_cloud_run_v2_service.default.name
   }
 }
 
