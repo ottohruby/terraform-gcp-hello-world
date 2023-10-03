1961ac560fec1286fb0b4ffaee32270236ed5908
ottohruby <70574289+ottohruby@users.noreply.github.com>
2023-10-03 13:05:56 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,8 @@
-variable "gcp_service_list"{
+data "google_project" "project" {
+    project_id= "otto-hruby-test"
+}
+
+variable "gcp_required_api"{
   type = list(string)
   default= [
     "cloudresourcemanager.googleapis.com",
@@ -8,18 +12,18 @@ variable "gcp_service_list"{
     "iam.googleapis.com"
     ]
 }
-resource "google_project_service" "gcp_services" {
-  for_each = toset(var.gcp_service_list)
-  project = "otto-hruby-test"
+resource "google_project_service" "gcp_api" {
+  for_each = toset(var.gcp_required_api)
+  project = data.google_project.project.project_id
   service = each.key
 }
 
 module "data_logger" {
   source = "./modules/data_logger"
-  depends_on = [google_project_service.gcp_services]
+  depends_on = [google_project_service.gcp_api]
 }
 
 module "pubsub-data-logger-events" {
   source = "./modules/pubsub-data-logger-events"
-  depends_on = [google_project_service.gcp_services]
+  depends_on = [google_project_service.gcp_api]
 }
